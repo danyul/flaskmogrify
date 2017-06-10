3b15a1757517e90dba2b1d4c2b83127be51f1dcc
Dan L <git.dan@langsam.org>
2017-06-10 12:00:16 -0400

    minor text change

--- run-example.py
+++ run-example.py
@@ -8,7 +8,7 @@ flaskmogrify.app.config['transmogrify_functions'][0].__name__ = "reversal"
 flaskmogrify.app.config['transmogrify_functions'][1].__name__ = "x_on_both_sides"
 
 flaskmogrify.app.config['transmogrify_sample_text'] =  \
-    "Lorum ipsum dolor sit amet. Copy-paste me into the textbox.)"
+    "Lorum ipsum dolor sit amet. Copy-paste me into the textbox."
 
 if __name__ == '__main__':
     flaskmogrify.app.run(debug=True,port=5001)
